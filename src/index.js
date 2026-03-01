bd3266c1a0edaa655bb0fb6129f67a8ed42a05cd
Alex Jarabi <jarabialex@gmail.com>
2026-03-01 19:32:53 +0300

    implement arcjet security

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { matchRouter } from './routes/matches.js';
 import { attachWebSocketServer } from './ws/server.js';
+import { securityMiddleware } from './arcjet.js';
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
@@ -11,6 +12,7 @@ const server = http.createServer(app);
 
 // Middleware
 app.use(express.json());
+app.use(securityMiddleware());
 app.use('/matches', matchRouter);
 
 // Root GET route
